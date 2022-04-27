a0f94c3310c6f8605ab5ee7f4c83de5312f4eba6
N1klbr1k <lemmonynikit@gmail.com>
2022-04-27 15:11:17 -0600

    trying stuff

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
-const express = require("express");
+const express = require('express');
 
-const path = require("path");
+const path = require('path');
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.sendFile.apply(path.join(__dirname, "../index.html"));
+  res.sendFile.apply(path.join(__dirname, '../index.html'));
 });
 
 const port = process.env.PORT || 4006;
